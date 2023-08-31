c9de50cdc9dcd76486f23bc833824e3382da1ec6
klaus Kindermann <klauskindermann94@gmail.com>
2023-08-31 10:49:59 -0300

    /health

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 import { CredentialsModule } from './credentials/credentials.module';
 import { NotesModule } from './notes/notes.module';
 import { CardsModule } from './cards/cards.module';
